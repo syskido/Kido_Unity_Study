5a555c8f09e807c157bd9c394b88ea71725c233a
kido <61651321+syskido@users.noreply.github.com>
2025-12-18 20:19:15 +0900

    그룹형변수[int] 결과

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -13,23 +13,18 @@ public class NewBehaviourScript : MonoBehaviour
         int level = 5;
         float strength = 15.5f;
         string playerName = "검사";
-        bool isFullLevel = false;
-
-        //Debug.Log("용사의 이름은 ?");
-        //Debug.Log(playerName);
-        //Debug.Log("용사의 레벨은?");
-        //Debug.Log(level);
-        //Debug.Log("용사의 힘은?");
-        //Debug.Log(strength);
-        //Debug.Log("용사는 만렙인가?");
-        //Debug.Log(isFullLevel);
+        bool isFullLevel = false;        
 
         //2.그룹형 변수
         string[] monsters = { "윤기도", "천사교통", "천사택시" };
+        int[] monsterLevel = new int[3];
+        monsterLevel[0] = 1;
+        monsterLevel[1] = 6;
+        monsterLevel[2] = 20;
 
-        Debug.Log("맵에 존재하는 몬스터");
-        Debug.Log(monsters[0]);
-        Debug.Log(monsters[1]);
-        Debug.Log(monsters[2]);
+        Debug.Log("맵에 존재하는 몬스터의 레벨");
+        Debug.Log(monsterLevel[0]);
+        Debug.Log(monsterLevel[1]);
+        Debug.Log(monsterLevel[2]);
     }  
 }
