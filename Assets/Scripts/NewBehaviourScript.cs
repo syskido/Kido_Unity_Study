79e065187ed6e4d91ab80147b538e092d2c5275b
kido <61651321+syskido@users.noreply.github.com>
2025-12-18 20:27:04 +0900

    그룹형 변수 List - string

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -16,15 +16,23 @@ public class NewBehaviourScript : MonoBehaviour
         bool isFullLevel = false;        
 
         //2.그룹형 변수
-        string[] monsters = { "윤기도", "천사교통", "천사택시" };
-        int[] monsterLevel = new int[3];
-        monsterLevel[0] = 1;
-        monsterLevel[1] = 6;
-        monsterLevel[2] = 20;
+        //string[] monsters = { "윤기도", "천사교통", "천사택시" };
+        //int[] monsterLevel = new int[3];
+        //monsterLevel[0] = 1;
+        //monsterLevel[1] = 6;
+        //monsterLevel[2] = 20;
 
-        Debug.Log("맵에 존재하는 몬스터의 레벨");
-        Debug.Log(monsterLevel[0]);
-        Debug.Log(monsterLevel[1]);
-        Debug.Log(monsterLevel[2]);
+        //Debug.Log("맵에 존재하는 몬스터의 레벨");
+        //Debug.Log(monsterLevel[0]);
+        //Debug.Log(monsterLevel[1]);
+        //Debug.Log(monsterLevel[2]);
+
+        List<string> items = new List<string>();
+        items.Add("생명물약30");
+        items.Add("마나물약30");
+
+        Debug.Log("가지고 있는 아이템");
+        Debug.Log(items[0]);
+        Debug.Log(items[1]);
     }  
 }
